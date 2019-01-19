b13ec13de8be3cace30aae0cf7162fa2b66b495a
xiatao <2761564455@qq.com>
2019-01-19 16:43:12 +0800

    美溪登录界面

--- UI_MainWindow.py
+++ UI_MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file 'UI_mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -8,41 +8,58 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_main_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(200, 130, 411, 71))
+        MainWindow.resize(550, 550)
+        MainWindow.setStyleSheet("#MainWindow {background-image:url(./bg2.jpg);}")
+        self.label = QtWidgets.QLabel(MainWindow)
+        self.label.setGeometry(QtCore.QRect(120, 20, 291, 41))
         font = QtGui.QFont()
-        font.setFamily("04b_21")
-        font.setPointSize(26)
+        font.setFamily("仿宋")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
         self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 243, 101);")
         self.label.setObjectName("label")
-        self.pushButton1 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton1.setGeometry(QtCore.QRect(160, 342, 75, 31))
-        self.pushButton1.setObjectName("pushButton")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(420, 310, 54, 12))
+        self.graphicsView = QtWidgets.QGraphicsView(MainWindow)
+        self.graphicsView.setGeometry(QtCore.QRect(0, 120, 241, 231))
+        self.graphicsView.setObjectName("graphicsView")
+        self.pushButton = QtWidgets.QPushButton(MainWindow)
+        self.pushButton.setGeometry(QtCore.QRect(0, 80, 81, 31))
+        self.pushButton.setStyleSheet("")
+        self.pushButton.setObjectName("pushButton")
+        self.textBrowser = QtWidgets.QTextBrowser(MainWindow)
+        self.textBrowser.setGeometry(QtCore.QRect(315, 120, 231, 231))
+        self.textBrowser.setObjectName("textBrowser")
+        self.label_2 = QtWidgets.QLabel(MainWindow)
+        self.label_2.setGeometry(QtCore.QRect(360, 80, 111, 31))
+        font = QtGui.QFont()
+        font.setFamily("仿宋")
+        font.setPointSize(20)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("background-color: rgb(255, 199, 85);")
         self.label_2.setObjectName("label_2")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(MainWindow)
+        self.commandLinkButton.setGeometry(QtCore.QRect(220, 360, 121, 41))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(14)
+        self.commandLinkButton.setFont(font)
+        self.commandLinkButton.setObjectName("commandLinkButton")
 
         self.retranslateUi(MainWindow)
-        self.pushButton1.clicked.connect(MainWindow.showLabel)
+        self.pushButton.clicked.connect(MainWindow.choose)
+        self.commandLinkButton.clicked.connect(MainWindow.recognition)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "登陆成功，进入主界面"))
-        self.pushButton1.setText(_translate("MainWindow", "播报语音"))
-        self.label_2.setText(_translate("MainWindow", "TextLabel"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "发票识别系统"))
+        self.label.setText(_translate("MainWindow", "欢迎使用发票识别系统"))
+        self.pushButton.setText(_translate("MainWindow", "选择发票"))
+        self.label_2.setText(_translate("MainWindow", "识别结果"))
+        self.commandLinkButton.setText(_translate("MainWindow", "开始识别"))
+
--- callfirstwindow.py
+++ callfirstwindow.py
@@ -1,24 +1,64 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from UI_Login import Ui_Login_MainWindow
-from main import *
+from UI_MainWindow import Ui_main_MainWindow
+from aip import  AipOcr
 import sys
+
+APP_ID = '14660084'
+API_KEY = 'liGXaa0wfIrlzTB1x5bT8Afc'
+SECRET_KEY = 'GHLpfGKOx54mRWHXKFruBSLtoWCT6yYB'
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+
+class Main_Window(QMainWindow, Ui_main_MainWindow):
+
+    def __init__(self):
+        super(Main_Window, self).__init__()  # 通过这句话就可以调用其父类的属性和方法
+        self.setupUi(self)
+
+    def get_file_content(self):
+        with open(self.file_path, 'rb') as fp:
+            print('finish get image')
+            return fp.read()
+
+    def choose(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "选择发票", r"C:\Users\jack xia\Desktop\test")
+        self.file_path = file_path
+        img = QImage()
+        img.load(file_path)  # 载入图片
+        self.img = img
+        self.img = img.scaled(self.graphicsView.width(), self.graphicsView.height())
+        self.graphicsView.scene = QGraphicsScene()  # 创建一个图片元素的对象
+        self.graphicsView.scene.addPixmap(QPixmap().fromImage(self.img))  # 将加载后的图片传递给scene对象
+        self.graphicsView.setScene(self.graphicsView.scene)
+        self.img = self.get_file_content()
+
+    def recognition(self):
+        print('---开始识别---')
+        data = client.vatInvoice(self.img)
+        print('----data----', data)
 
 
 class Login_Window(QMainWindow, Ui_Login_MainWindow):
+
     def __init__(self):
         super(Login_Window, self).__init__()  # 通过这句话就可以调用其父类的属性和方法
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.slot1)
 
     def slot1(self):
-        if self.lineEdit.text() == 'xt' and self.lineEdit_2.text() == '123':
-            self.close()
-            mainwindow.show()
+        if self.lineEdit_4.text() == '' and self.lineEdit_3.text() == '':
+            self.hide()
+            self.main = Main_Window()
+            self.main.show()
+        else:
+            QMessageBox.critical(self, "Login Error！", "用户名为：meixi,密码为：521")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     login_window = Login_Window()  # 初始化
     login_window.show()
-    mainwindow = Main_Window()
     sys.exit(app.exec_())
